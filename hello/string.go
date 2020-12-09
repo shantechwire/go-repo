c6f5bbdca163224fb4da11ef240e94268cfa71fc
shantechwire <shantechwire@gmail.com>
2020-12-09 21:38:13 +0000

    go-commit

---
+++ hello/string.go
@@ -0,0 +1,10 @@
+packages string
+
+func Reverse(s string) string {
+		b:= []rune(s)
+			for i := 0; i < len(be)/2; i++ {
+						J := len(b)-i-1
+								b[i], b[j] = b[j], b[i]
+									}
+										return string(b)
+									}
